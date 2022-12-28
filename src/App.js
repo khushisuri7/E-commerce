058dbe8b45546234f8898d7f60b63a86cf42ec2b
khushi <khushisuri9252gmail.com>
2022-12-28 18:38:08 +0530

    Database formed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Nav from './components/Nav';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Footer from './components/Footer'
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route path="/update" element={<h1>Update Product Component</h1>}/>
         <Route path="/logout" element={<h1>Logout Component</h1>}/>
         <Route path="/profile" element={<h1>Profile Component</h1>}/>
+        <Route path="/signup" element={<SignUp/>}/>
       </Routes>
       <Footer/>
       </BrowserRouter>
